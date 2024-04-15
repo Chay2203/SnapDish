19855f523d7f05c4cd4bcad221f3a849c1a55757
Naga Chaitanya Varma <naga@Nagas-MacBook-Air.local>
2024-04-15 09:41:56 +0530

    changed name

--- components/snapcook.js
+++ components/snapcook.js
@@ -8,7 +8,7 @@ export default function SnapCook() {
         <View className="bg-yellow-300 flex-row items-center p-3 rounded-xl mt-2 mx-6">
             <View className="flex-1">
             <Text className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-900 tracking-tight">
-                SnapCook 
+                SnapDish
             </Text>
                 <Text className="text-sm text-gray-600 font-semibold italic">
                     Get recipes based on ingredients
